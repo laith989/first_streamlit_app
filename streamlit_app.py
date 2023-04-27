84fb7d7fac80a25c7ed526b7b942ba5d26fa8a27
Layth Al-Ani <laith989@users.noreply.github.com>
2023-04-27 15:43:05 -0400

    Update streamlit_app.py
    Styling

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
-streamlit.header('What fruit would like to add?')
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
